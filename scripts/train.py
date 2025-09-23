48b4a0b5e04f0ad497270c13a89bdf000930bc34
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-09-22 23:12:19 -0230

    chaniging backend to eager

--- scripts/train.py
+++ scripts/train.py
@@ -86,8 +86,10 @@ config_keys = [
     for k, v in globals().items()
     if not k.startswith("_") and isinstance(v, (int, float, bool, str, list))
 ]
+print("1")
 exec(open("configurator.py").read())  # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys}  # will be useful for logging
+print("2")
 
 # -----------------------------------------------------------------------------
 
