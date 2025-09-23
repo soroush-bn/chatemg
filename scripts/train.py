ddc90da344b9ed076967b4a6f878247a5642f8f4
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-09-22 22:31:38 -0230

    chaniging backend to eager

--- scripts/train.py
+++ scripts/train.py
@@ -199,7 +199,7 @@ checkpoint = None  # free up memory
 if compile:
     print("compiling the model... (takes a ~minute)")
     unoptimized_model = model
-    model = torch.compile(model)  # requires PyTorch 2.0
+    model = torch.compile(model,backend="eager")  # requires PyTorch 2.0
 
 
 # helps estimate an arbitrarily accurate loss over either split using many batches
