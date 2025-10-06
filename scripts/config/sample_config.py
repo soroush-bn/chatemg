6a82af732f5b730a354db361abb5069104dbe94e
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-10-06 09:40:56 -0230

    change class label

--- scripts/config/sample_config.py
+++ scripts/config/sample_config.py
@@ -1,5 +1,5 @@
 exp_name = "sample_train"
-filter_class = 0  # [relax, open, close]
+filter_class = 15  # [relax, open, close]
 eval_interval = 1000  # checkpoint interval, how often the checkpoint is saved
 eval_iters = 200
 log_interval = 10  # don't print too often
