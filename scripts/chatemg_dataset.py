f719d26b1a51b9bdb155e35e1908b481c7b1baf7
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-11-05 12:57:49 -0330

    debug sample.py

--- scripts/chatemg_dataset.py
+++ scripts/chatemg_dataset.py
@@ -43,6 +43,8 @@ class ChatEMGDataset(Dataset):
         for f in self.csv_files:
             data_path = f
             df = pd.read_csv(data_path, index_col=0)
+            print("11111111111111111111111111")
+            print(df['gt'].unique())
             # its just a workaround for now 
             if which_file == "train":
                 # get 0 to 90% of data
@@ -52,6 +54,9 @@ class ChatEMGDataset(Dataset):
                 #get the last 10% of data
                 split_index = int(0.9 * len(df))
                 df = df.iloc[split_index:]
+            print("222222222222222222222222222")
+            print(df['gt'].unique())
+            
             #i want to see the type of gt values
             # print(df['gt'].dtype)
             X, y = mu.clean_dataframe(df,sensor_type,location)
