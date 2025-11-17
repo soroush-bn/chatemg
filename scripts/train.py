bdccad096076779e7025587ee940cd6fc9d5a780
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-11-17 11:33:32 -0330

    adding same proj name for wandb

--- scripts/train.py
+++ scripts/train.py
@@ -145,7 +145,7 @@ if master_process:
     os.makedirs(save_dir, exist_ok=True)
 
 
-shutil.copy(args.config, os.path.join(save_dir, args.config))
+shutil.copy(args.config, os.path.join(save_dir, os.path.basename(args.config)))
 
 np.random.seed(1337 + seed_offset)  # dataset is using numpy
 torch.manual_seed(1337 + seed_offset)
