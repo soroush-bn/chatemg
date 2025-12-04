5a217b88d29aac3855428f016835776c7a2b662b
soroush-bn <soroush.baghernezhad3@gmail.com>
2025-12-04 16:29:48 -0330

    adding extra comments for debugging

--- scripts/misc_utils.py
+++ scripts/misc_utils.py
@@ -15,6 +15,9 @@ from_int_to_str = {0: "relax", 1: "open", 2: "close"}
 from_str_to_int = {"relax": 0, "open": 1, "close": 2}
 
 EMG_COLORS = ["b", "g", "r", "c", "m", "y", "k", "tab:brown"]
+SAMPLING_FREQ = 2000
+GESTURE_DURATION_SEC = 2
+NUMBER_OF_REPEATS = 4
 
 
 def plot_emg_chunks_parallel(
@@ -186,6 +189,7 @@ def medfilt_wo_padding(arr, window_size):
     e = -s
     return medfilt(arr, [1, window_size, 1])[:, s:e, :]
 
+def 
 
 def sample_from_dataset(dataset, num, replace=False):
     # sample some samples without replacement
