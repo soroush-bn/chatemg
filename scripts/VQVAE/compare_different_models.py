ec11d656c400900dfefde32a29d9e77bd0d40808
soroush-bn <soroush.baghernezhad3@gmail.com>
2026-01-29 15:48:24 -0330

    changing base module

---
+++ scripts/VQVAE/compare_different_models.py
@@ -0,0 +1,4 @@
+#this file got the script to compare and pick the best VQVAE mdoel given the path of saved models. 
+
+def compare_different_models(model_paths, dataloader, device, config):
+    pass 
